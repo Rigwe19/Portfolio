74b2d1a6e0098267f663adbe5fc8b92bde869399
Igwe Reinhard <108090171+Rigwe19@users.noreply.github.com>
2024-02-14 09:54:45 +0100

    made experience dynamic

--- src/Components/About.tsx
+++ src/Components/About.tsx
@@ -1,18 +1,19 @@
 import React from 'react'
 import hero from '../assets/img.png';
 type Props = {}
+    let currentYear = new Date().getFullYear() - 2017;
 
 const About = (props: Props) => {
     return (
         <section id='about'>
-            <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
-                <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-                    <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
+            <div className="container flex flex-col items-center px-10 py-20 mx-auto md:flex-row">
+                <div className="flex flex-col items-center mb-16 text-center lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 md:items-start md:text-left md:mb-0">
+                    <h1 className="mb-4 text-3xl font-medium text-white title-font sm:text-4xl">
                         Hi, I'm Reinhard
                         <br className="hidden lg:inline-block" /> I love to build amazing apps.
                     </h1>
                     <p className="mb-8 leading-relaxed text-justify">
-                        I am a fullstack web developer with 5 years+ experience PHP and the Laravel
+                        I am a fullstack web developer with { currentYear } years+ experience PHP and the Laravel
                         framework, Javascript with the following frameworks, ReactJs, VueJs
                         and NodeJs and its frameworks expressJs and NextJs, i also have experience 
                         in electronJs for cross platform desktops app and Ionic for cross platform
@@ -20,11 +21,11 @@ const About = (props: Props) => {
                         or i use InertiaJs for the monolithic approach
                     </p>
                     <div className="flex justify-center">
-                        <a href="#contact" className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 hover:text-green-100 rounded text-lg">Work With Me</a>
-                        <a href="#projects" className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">See My Previous Works</a>
+                        <a href="#contact" className="inline-flex px-6 py-2 text-lg text-white bg-green-500 border-0 rounded focus:outline-none hover:bg-green-600 hover:text-green-100">Work With Me</a>
+                        <a href="#projects" className="inline-flex px-6 py-2 ml-4 text-lg text-gray-400 bg-gray-800 border-0 rounded focus:outline-none hover:bg-gray-700 hover:text-white">See My Previous Works</a>
                     </div>
                 </div>
-                <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 hero-img">
+                <div className="w-5/6 lg:max-w-lg lg:w-full md:w-1/2 hero-img">
                     <img src={hero} alt="hero" className="object-cover object-center rounded" />
                 </div>
             </div>
